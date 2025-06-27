43e66f91e6c9fb21d5c6372ec18e8d3edf2af9b2
gabaxh <gabriel.axheim@outlook.com>
2025-06-27 17:16:06 +0200

    Started working on tests for usecases/provision.go

--- usecases/provision_test.go
+++ usecases/provision_test.go
@@ -1,23 +1,42 @@
 package usecases
 
 import (
+	"encoding/xml"
+	"fmt"
 	"io"
 	"net/http"
-	"net/http/httptest"
 	"strings"
-	"testing"
 
 	"github.com/sdoque/mbaigo/forms"
 )
 
 type httpProcessGetRequestStruct struct {
-	inputW           http.ResponseWriter
-	inputR           *http.Request
-	inputF           forms.Form
-	body             func() *http.Response
-	mockTransportErr int
-	errHTTP          error
-	testName         string
+	inputW      http.ResponseWriter
+	inputR      *http.Request
+	inputF      forms.Form
+	expectedErr error
+	testName    string
+}
+
+type brokenTestValueForm struct {
+	XMLName xml.Name  `json:"-" xml:"testName"`
+	Value   complex64 `json:"value" xml:"value"`
+	Unit    string    `json:"unit" xml:"unit"`
+	Version string    `json:"version" xml:"version"`
+}
+
+type mockResponseWriter struct {
+	http.ResponseWriter
+}
+
+func (e *mockResponseWriter) Write(b []byte) (int, error) {
+	return 0, fmt.Errorf("Forced write error")
+}
+
+func (e *mockResponseWriter) WriteHeader(statusCode int) {}
+
+func (e *mockResponseWriter) Header() http.Header {
+	return make(http.Header)
 }
 
 func createHttpResponse() func() *http.Response {
@@ -38,15 +57,44 @@ func createEmptyFormVersion() forms.Form {
 	return &form
 }
 
+func (f brokenTestValueForm) NewForm() forms.Form {
+	f.Version = "testVersion"
+	return f
+}
+
+func (f brokenTestValueForm) FormVersion() string {
+	return f.Version
+}
+
+func createBrokenForm() brokenTestValueForm {
+	form := brokenTestValueForm{
+		XMLName: xml.Name{},
+		Value:   complex(1, 2),
+		Unit:    "testUnit",
+		Version: "testVersion",
+	}
+	return form
+}
+
+var mockError error = fmt.Errorf("A mock error")
+
+// TODO: Uncomment this test if we get approval to make it so that HTTPProcessGetRequest returns an error, also make sure the expectedErr gets it proper value then
+/*
 var httpProcessGetRequestParams = []httpProcessGetRequestStruct{
-	{httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/test123", nil), form.NewForm(), createHttpResponse(), 0, nil, "Good case"},
-	{httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/test123", nil), nil, createHttpResponse(), 0, nil, "Bad case, form is nil"},
-	{httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/test123", nil), createEmptyFormVersion(), createHttpResponse(), 0, nil, "Bad case, form version is empty"},
-	{httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/test123", nil), form.NewForm(), createHttpResponse(), 0, nil, "Good case"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/test123", nil), form.NewForm(), nil, "Good case"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/test123", nil), nil, mockError, "Bad case, form is nil"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/test123", nil), createEmptyFormVersion(), mockError, "Bad case, form version is empty"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodGet, "/test123", nil), createBrokenForm(), mockError, "Bad case, form value is invalid"},
+	{&mockResponseWriter{}, httptest.NewRequest(http.MethodGet, "/test123", nil), form.NewForm(), mockError, "Bad case, Write fails"},
 }
 
 func TestHTTPProcessGetRequest(t *testing.T) {
 	for _, testCase := range httpProcessGetRequestParams {
-		HTTPProcessGetRequest(testCase.inputW, testCase.inputR, testCase.inputF)
+		err := HTTPProcessGetRequest(testCase.inputW, testCase.inputR, testCase.inputF)
+
+		if err != testCase.expectedErr {
+			t.Errorf("Expected %v, got: %v", testCase.expectedErr, err)
+		}
 	}
 }
+*/
