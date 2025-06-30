1d2c2d14ee5931d4cf75ddaedf063d0c524d8a65
gabaxh <gabriel.axheim@outlook.com>
2025-06-30 14:34:39 +0200

    Made tests for HTTPProcessSetRequest and getBestContentType

--- usecases/provision_test.go
+++ usecases/provision_test.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/http/httptest"
 	"strings"
+	"testing"
 
 	"github.com/sdoque/mbaigo/forms"
 )
@@ -57,21 +59,12 @@ func createEmptyFormVersion() forms.Form {
 	return &form
 }
 
-func (f brokenTestValueForm) NewForm() forms.Form {
-	f.Version = "testVersion"
-	return f
-}
-
-func (f brokenTestValueForm) FormVersion() string {
-	return f.Version
-}
-
 func createBrokenForm() brokenTestValueForm {
 	form := brokenTestValueForm{
 		XMLName: xml.Name{},
 		Value:   complex(1, 2),
 		Unit:    "testUnit",
-		Version: "testVersion",
+		Version: "SignalA_v1.0",
 	}
 	return form
 }
@@ -98,3 +91,91 @@ func TestHTTPProcessGetRequest(t *testing.T) {
 	}
 }
 */
+
+type httpProcessSetRequestStruct struct {
+	inputW       http.ResponseWriter
+	inputR       *http.Request
+	expectedErr  error
+	expectedForm forms.SignalA_v1a
+	testName     string
+}
+
+func createForm() forms.SignalA_v1a {
+	form.NewForm()
+	return form
+}
+
+func createBody() io.ReadCloser {
+	return io.NopCloser(strings.NewReader(string("{\n  \"value\": 0,\n  \"unit\": \"\",\n  \"timestamp\": \"0001-01-01T00:00:00Z\",\n  \"version\": \"SignalA_v1.0\"\n}")))
+}
+
+func createBrokenBody() io.ReadCloser {
+	return io.NopCloser(strings.NewReader(string([]byte{0})))
+}
+
+func createBodyWithNoformVersion() io.ReadCloser {
+	return io.NopCloser(strings.NewReader(string("{\n  \"value\": 0,\n  \"unit\": \"\",\n  \"timestamp\": \"0001-01-01T00:00:00Z\",\n  \"bersion\": \"SignalA_v1.0\"\n}")))
+}
+
+func createBodyWithWrongForm() io.ReadCloser {
+	return io.NopCloser(strings.NewReader(string("{\n  \"value\": \"not-a-number\",\n  \"unit\": \"\",\n  \"timestamp\": \"0001-01-01T00:00:00Z\",\n  \"version\": \"SignalA_v1.0\"\n}")))
+}
+
+func createBodyWithWrongFormVersion() io.ReadCloser {
+	return io.NopCloser(strings.NewReader(string("{\n  \"value\": 0,\n  \"unit\": \"\",\n  \"timestamp\": \"0001-01-01T00:00:00Z\",\n  \"version\": \"SignalB_v1.0\"\n}")))
+}
+
+func errorIsSame(err1 error, err2 error) bool {
+	switch {
+	case err1 == nil && err2 == nil:
+		return true
+	case err1 == nil || err2 == nil:
+		return false
+	default:
+		return true
+	}
+}
+
+var httpProcessSetRequestParams = []httpProcessSetRequestStruct{
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, "/test123", createBody()), nil, createForm(), "Good case"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, "/test123", io.NopCloser(errorReader{})), errBodyRead, forms.SignalA_v1a{}, "Bad case, ReadAll returns error"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, "/test123", createBrokenBody()), errBodyRead, forms.SignalA_v1a{}, "Bad case, Unmarshal returns error"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, "/test123", createBodyWithNoformVersion()), nil, forms.SignalA_v1a{}, "Bad case, version key missing"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, "/test123", createBodyWithWrongForm()), errBodyRead, forms.SignalA_v1a{}, "Bad case, Second Unmarshal breaks"},
+	{httptest.NewRecorder(), httptest.NewRequest(http.MethodPut, "/test123", createBodyWithWrongFormVersion()), errBodyRead, forms.SignalA_v1a{}, "Bad case, version is wrong"},
+}
+
+func TestHTTPProcessSetRequest(t *testing.T) {
+	for _, testCase := range httpProcessSetRequestParams {
+		f, err := HTTPProcessSetRequest(testCase.inputW, testCase.inputR)
+
+		if f != testCase.expectedForm || !errorIsSame(err, testCase.expectedErr) {
+			t.Errorf("Expected %v and %v, got: %v and %v", testCase.expectedForm, testCase.expectedErr, f, err)
+		}
+	}
+}
+
+type getBestContentTypeStruct struct {
+	acceptHeaderInput     string
+	bestContentTypeOutput string
+	testName              string
+}
+
+var getBestContentTypeParams = []getBestContentTypeStruct{
+	{"", "application/json", "Good case, no accept header provided"},
+	{"application/xml", "application/xml", "Good case, accept header provided without q-values"},
+	{"application/xml;q=0.7, application/json;q=0.9", "application/json", "Good case, accept header provided with q-values"},
+	{"application/xml;q=wrong, application/json;q=1.1", "application/json", "Good case, xml gets skipped"},
+	{"application/xml;q=0.9, application/json;q=0.9", "application/xml", "Good case, equal q-values selects the first one"},
+	{"application/xml;q=-0.9", "application/json", "Good case, no MIME type found"},
+}
+
+func TestGetBestContentType(t *testing.T) {
+	for _, testCase := range getBestContentTypeParams {
+		res := getBestContentType(testCase.acceptHeaderInput)
+
+		if res != testCase.bestContentTypeOutput {
+			t.Errorf("Expected %v, got: %v in test case: %s", testCase.bestContentTypeOutput, res, testCase.testName)
+		}
+	}
+}
